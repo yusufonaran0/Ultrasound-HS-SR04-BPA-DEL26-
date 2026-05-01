48b91f714872c064bf7655e1030a9e3ecac0c54d
Yusuf Onaran <164936141+yusufonaran0@users.noreply.github.com>
2026-05-01 20:03:28 +0300

    Update bin2seg.v

--- vivado/ultrasonic_1.srcs/sources_1/new/bin2seg.v
+++ vivado/ultrasonic_1.srcs/sources_1/new/bin2seg.v
@@ -1,10 +1,41 @@
+// -----------------------------------------------------------
+//! @brief Binary to 7-segment decoder (common anode, 1 digit)
+//! @version 2.1
+//! @copyright (c) 2018-2026 Tomas Fryza, MIT license
+//!
+//! This module decodes a 4-bit binary input into control signals
+//! for a 7-segment common-anode display. It supports hexadecimal
+//! characters.
+//!
+//!     --a--
+//!    |     |
+//!    f     b
+//!    |     |
+//!     --g--
+//!    |     |
+//!    e     c
+//!    |     |
+//!     --d--
+//
+// Notes:
+// - Common anode: segment ON = 0, OFF = 1
+// - No decimal point is implemented
+// - Purely combinational (no clock)
+// -----------------------------------------------------------
+
 `timescale 1ns/1ps
 
 module bin2seg (
-    input  wire [3:0] bin,
-    output reg  [6:0] seg
+    input  wire [3:0] bin,  //! 4-bit input
+    output reg  [6:0] seg   //! {a,b,c,d,e,f,g} active-low
+    // Because `o_seg` is assigned inside an always block (see
+    // below), it must be declared as `reg` in Verilog. This does
+    // NOT mean it becomes a flip-flop or register in hardware.
 );
 
+    //! This describes a combinational logic process.
+    //! The @(*) is the sensitivity list. It means: Trigger
+    //! this block whenever any signal used inside the block changes.
     always @(*) begin
         case (bin)
             4'h0: seg = 7'b000_0001;
@@ -23,7 +54,8 @@ module bin2seg (
             4'hD: seg = 7'b100_0010;
             4'hE: seg = 7'b011_0000;
             4'hF: seg = 7'b011_1000;
-            default: seg = 7'b111_1111;
+
+            default: seg = 7'b111_1111;  // Blank for safety
         endcase
     end
 
